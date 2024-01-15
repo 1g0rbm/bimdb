2fec8369a10aa953046590c8c59faa6fcec0adce
1g0rbm <m1g0rb89@gmail.com>
2024-01-15 23:50:15 +0300

    Added mutex.

--- internal/database/storage/engine/in_memory/engine.go
+++ internal/database/storage/engine/in_memory/engine.go
@@ -1,6 +1,9 @@
 package in_memory
 
+import "sync"
+
 type Engine struct {
+	mu    sync.RWMutex
 	store map[string]string
 }
 
@@ -11,15 +14,24 @@ func NewEngine() *Engine {
 }
 
 func (e *Engine) Set(key string, value string) {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+
 	e.store[key] = value
 }
 
 func (e *Engine) Get(key string) (string, bool) {
+	e.mu.RLock()
+	defer e.mu.Unlock()
+
 	value, ok := e.store[key]
 
 	return value, ok
 }
 
 func (e *Engine) Del(key string) {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+
 	delete(e.store, key)
 }
